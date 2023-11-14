98c7818fc3970b244e2241f059a54169c1cfef2e
IvP <vanusezzz@gmail.com>
2023-11-14 10:46:51 +0300

    write cycles on the ArrayListCollections.java & QueueCollection.java

--- src/main/java/qaQuru22/ArrayListCollections.java
+++ src/main/java/qaQuru22/ArrayListCollections.java
@@ -1,5 +1,45 @@
 package qaQuru22;
 
-public class ArrayListCollections {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ArrayListCollections {
+        private static final List<Integer> list = new ArrayList<>();
+
+        public static void addList(int A) {
+            list.add(A);
+        }
+
+        public static void findElements(int A){
+            if (list.contains(A))
+                System.out.println("Элемент " + A + " найден");
+            else
+                System.out.println("Элемент " + A + " не найден");
+        }
+
+        public static void removeElement(int A){
+            int i = 0 ;
+            while (i < list.size()){
+                if (list.get(i).equals(A))
+                    list.remove(i);
+                i++;
+            }
+        }
+
+
+        public static void main (String[] arg) {
+            for (int i = 0; i < 10; i++) {
+                addList(i);
+            }
+
+
+            findElements(4);
+            findElements(13);
+
+            removeElement(1);
+            removeElement(6);
+            removeElement(8);
+
+            System.out.println(list);
+        }
 }
--- src/main/java/qaQuru22/QueueCollection.java
+++ src/main/java/qaQuru22/QueueCollection.java
@@ -1,6 +1,44 @@
 package qaQuru22;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class QueueCollection {
 
-    // Queue - очередь
+    // Queue - очередь ; DEQUE - двухсторонняя очередь
+
+    private static final Queue<Integer> queue = new ArrayDeque<>();
+
+    public static void addQueue(int B){
+        queue.add(B);
+        if (queue.contains(B))
+            System.out.println("Find " + B + " element");
+        else
+            System.out.println("Didn't find " + B + " element");
+    }
+
+    public static void removeQueue(int B) {
+        queue.remove(B);
+    }
+
+    public static void main (String[] args) {
+        for (int i = 0 ; i < 10 ; i++){
+            addQueue(i);
+        }
+
+        addQueue(1);
+        addQueue(9);
+                    // System.out.println(queue);
+        removeQueue(7);
+        removeQueue(1);
+        removeQueue(4);
+                    // System.out.println(queue);
+
+        for (int num : queue){
+            System.out.println(num + " , ");
+        }
+    }
+
+
+
 }
