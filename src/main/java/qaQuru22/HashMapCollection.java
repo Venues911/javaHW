8f0adf39005c37f24226dbf9d142a7a7ffd26d31
IvP <vanusezzz@gmail.com>
2023-11-15 12:04:09 +0300

    write cycles on the HashMapCollection.java & HashSetCollection.java

--- src/main/java/qaQuru22/HashMapCollection.java
+++ src/main/java/qaQuru22/HashMapCollection.java
@@ -1,4 +1,45 @@
 package qaQuru22;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapCollection {
+
+    private static final Map<Integer, String> map = new HashMap<>();
+
+
+    public static void addMap (Integer a, String b) {
+        map.put(a, b);
+    }
+
+    public static void findMapElement(Integer a){
+        if (map.containsKey(a))
+            System.out.println("Find " + a + " key");
+        else
+            System.out.println("Didn't find " + a + " key");
+    }
+
+    public static void removeMap(int a){
+        map.remove(a);
+    }
+
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        while ( i < 10) {
+            addMap( i , "someWord");
+            i++;
+        }
+
+        findMapElement(7);
+        findMapElement(77);
+
+        removeMap(3);
+        removeMap(9);
+        removeMap(7);
+
+        System.out.println(map.entrySet());
+    }
+
 }
--- src/main/java/qaQuru22/HashSetCollection.java
+++ src/main/java/qaQuru22/HashSetCollection.java
@@ -1,4 +1,44 @@
 package qaQuru22;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetCollection {
+
+    private static Set<Integer> setMethod = new HashSet<>();
+
+    public static void addSet(int c){
+        setMethod.add(c);
+    }
+
+    public static void findElement(int c) {
+        if (setMethod.contains(c))
+            System.out.println("Find " + c + " element");
+        else
+            System.out.println("Didn't find " + c + " element");
+    }
+
+
+
+    public static void removeSetElement(int c) {
+        setMethod.remove(c);
+    }
+
+    public static void main(String[] args) {
+        int i = 0 ;
+
+        do {
+            addSet(i);
+            i++;
+        } while (i < 10);
+
+        findElement(3);
+        findElement(77);
+
+        removeSetElement(1);
+        removeSetElement(3);
+        removeSetElement(9);
+
+        System.out.println(setMethod);
+    }
 }
